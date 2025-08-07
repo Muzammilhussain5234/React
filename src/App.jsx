624a987688b9a7394c25baee0ab5c3eea9b2f5e5
Muzammilhussain5234 <muzammilhussain5473@gmail.com>
2025-08-07 20:07:29 +0500

    Initial commit from Day2 project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(10)
+  
+  
+
+  return (
+    <>
+       
+      
+ 
+        <button onClick={() => setCount((count) => count+111)}>
+          count is {count}
+        </button>
+        
+     
+    </>
+  )
+}
+
+export default App
