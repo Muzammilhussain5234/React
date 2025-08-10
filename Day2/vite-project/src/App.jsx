3bb4a37689b5ca2867862b52d6dd16d6ed23881d
Muzammilhussain5234 <muzammilhussain5473@gmail.com>
2025-08-10 21:17:10 +0500

    Organized project into react-app folder

---
+++ Day2/vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/footer.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar/>
+    <Footer/>
+    
+    
+    
+    </>
+  )
+}
+
+export default App
---
+++ Day2/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>spider</li>
+            </ul>
+        </nav>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
