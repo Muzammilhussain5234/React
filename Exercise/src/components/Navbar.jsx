84c64a19c9d92dc3766722b54905c768f5ce720c
Muzammilhussain5234 <muzammilhussain5473@gmail.com>
2025-08-20 21:58:25 +0500

    Add Exercise folder

---
+++ Exercise/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'><ul>
+        <li>HOME</li>
+        <li>CONTACT</li>
+        <li>ABOUT</li>
+        
+        
+        </ul></div>
+  )
+}
+
+export default Navbar
